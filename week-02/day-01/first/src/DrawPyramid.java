363db10be3cdff2c07fa787c00a0fc66b9f4f2fd
nkriszti92 <nemethkrisztina1992@gmail.com>
2018-04-16 22:14:41 +0200

    new week added

---
+++ week-02/day-01/first/src/DrawPyramid.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DrawPyramid {
+    // Write a program that reads a number from the standard input, then draws a
+// pyramid like this:
+//
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as the number was
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add an integer! ");
+
+        int rows = scanner.nextInt();
+        int temp = rows;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int c = 1; c < temp; c++)
+                System.out.print(" ");
+                temp--;
+
+            for (int c = 1; c <= 2 * i - 1; c++)
+                System.out.print("*");
+                System.out.println("");
+        }
+
+
+    }
+}
---
+++ week-02/day-01/first/src/PrintBigger.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrintBigger {
+    // Write a program that asks for two numbers and prints the bigger one
+
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Add a number! ");
+            int num1 = scanner.nextInt();
+
+            System.out.println("Add another number! ");
+            int num2 = scanner.nextInt();
+
+            if (num1 > num2) {
+                System.out.println("The bigger is: " + num1);
+            } else System.out.println("The bigger is: " + num2);
+        }
+
+    }
+
+
+
