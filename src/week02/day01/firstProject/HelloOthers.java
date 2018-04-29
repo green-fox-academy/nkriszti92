0f76d7336cbdf653b4e706a9e8baa7a24ccb5dad
nkriszti92 <nemethkrisztina1992@gmail.com>
2018-04-29 22:22:44 +0200

    week01 readded

--- src/week02/day01/firstProject/HelloOthers.java
+++
@@ -1,7 +0,0 @@
-public class HelloOthers {
-    public static void main(Strings[] args) {
-        System.out.println("Hello, Betti!");
-        System.out.println("Hello, Kinga!");
-        System.out.println("Hello, Evelin!");
-    }
-}
