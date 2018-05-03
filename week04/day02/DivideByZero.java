6ff645b707e55dc1957dc9af7476ba6ec8c24378
nkriszti92 <nemethkrisztina1992@gmail.com>
2018-05-03 20:10:00 +0200

    comments deleted

--- week04/day02/DivideByZero.java
+++ week04/day02/DivideByZero.java
@@ -13,11 +13,11 @@ public class DivideByZero {
         System.out.println("10/");
         int divisor = scanner.nextInt();
 
-        try { // Prevents the program breaking when attempting dividing by zero
-            int result = 10 / divisor; // If the input value for divisor was 0 it stops the try block
-            System.out.println("10/" +divisor +"="+ result); // The program doesn't reach this line if the input was 0
+        try {
+            int result = 10 / divisor;
+            System.out.println("10/" +divisor +"="+ result);
         } catch (ArithmeticException e) {
-            System.out.println("fail"); // This line only runs if the input was 0
+            System.out.println("fail"); 
         }
     }
 }
