16d0511e6e4994c8febcac6fe435af2ce5bdaaf3
nkriszti92 <nemethkrisztina1992@gmail.com>
2018-05-07 20:02:14 +0200

    Animal added

---
+++ week05/day01/Animal/src/Animal.java
@@ -0,0 +1,22 @@
+public class Animal {
+    int hunger;
+    int thirst;
+
+    public Animal() {
+        this.hunger = 50;
+        this.thirst = 50;
+    }
+
+    public void eat() {
+        this.hunger -= 1;
+    }
+
+    public void drink() {
+        this.thirst -= 1;
+    }
+
+    public void play() {
+        this.hunger -= 1;
+        this.thirst -= 1;
+    }
+}
