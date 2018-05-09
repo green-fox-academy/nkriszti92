dc455db878fe830a4c422cfa0901b7b3e5343e87
nkriszti92 <nemethkrisztina1992@gmail.com>
2018-05-09 20:24:49 +0200

    Teacher Student added

---
+++ week05/day01/TeacherStudent/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public void learn() {
+        System.out.println("I wanna learn!"); //Said no one ever :D
+    }
+
+    public void ask(Teacher teacher) {
+        System.out.println("But why?");
+        teacher.answer();
+    }
+}
---
+++ week05/day01/TeacherStudent/src/Teacher.java
@@ -0,0 +1,12 @@
+public class Teacher {
+    public void teach(Student student) {
+        student.learn();
+        System.out.println("One plus one is two!");
+
+    }
+
+    public void answer() {
+        System.out.println("Just because!");
+    }
+}
+
