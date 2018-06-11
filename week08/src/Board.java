99c1c2eb90e527f996db5635a5a35afd5c95da5b
nkriszti92 <nemethkrisztina1992@gmail.com>
2018-06-11 19:30:46 +0200

    Add HelloWorld

--- week08/src/Board.java
+++ week08/src/Board.java
@@ -1,27 +1,25 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class Board extends JComponent implements KeyListener {
 
-    int bossX = 0;
-    int bossY = 0;
+    int heroX = 0;
+    int heroY = 0;
     int increment = 72;
     int boardSize = 720;
     int tileSize = 72;
     int row = 0;
-    int coulumn = 0;
-    int skelPoz1x = ((int)(Math.random() * (9 - 1 + 1)) + 1);
-    int skelPoz1y = ((int)(Math.random() * (9 - 1 + 1)) + 1);
-    int skelPoz2x = ((int)(Math.random() * (9 - 1 + 1)) + 1);
-    int skelPoz2y = ((int)(Math.random() * (9 - 1 + 1)) + 1);
-    int skelPoz3x = ((int)(Math.random() * (9 - 1 + 1)) + 1);
-    int skelPoz3y = ((int)(Math.random() * (9 - 1 + 1)) + 1);
+    int column = 0;
+    int skelPoz1x = ((int)(Math.random() * (9 - 3 + 1)) + 3);
+    int skelPoz1y = ((int)(Math.random() * (9 - 3 + 1)) + 3);
+    int skelPoz2x = ((int)(Math.random() * (9 - 3 + 1)) + 3);
+    int skelPoz2y = ((int)(Math.random() * (9 - 3 + 1)) + 3);
+    int skelPoz3x = ((int)(Math.random() * (9 - 3 + 1)) + 3);
+    int skelPoz3y = ((int)(Math.random() * (9 - 3 + 1)) + 3);
+    int bossPozX = ((int)(Math.random() * (9 - 3 + 1)) + 3);
+    int bossPozY = ((int)(Math.random() * (9 - 3 + 1)) + 3);
 
     int[][] map = {
             {1, 1, 1, 0, 1, 0, 1, 1, 1, 1},
@@ -36,7 +34,6 @@ public class Board extends JComponent implements KeyListener {
             {1, 1, 1, 0, 1, 0, 0, 1, 0, 1},
     };
 
-
     public Board() {
 
 
@@ -45,8 +42,10 @@ public class Board extends JComponent implements KeyListener {
         setVisible(true);
     }
 
+
     @Override
     public void paint(Graphics graphics) {
+
         super.paint(graphics);
        /* graphics.fillRect(testBoxX, testBoxY, 100, 100);
         // here you have a 720x720 canvas
@@ -81,30 +80,38 @@ public class Board extends JComponent implements KeyListener {
 
         }
 
-        PositionedImage image = new PositionedImage("img/hero-down.png", bossX, bossY);
+
+        PositionedImage image = new PositionedImage("img/hero-down.png", heroX, heroY);
         image.draw(graphics);
 
 
-        while (map [skelPoz1y][skelPoz1x]== 0 ) {
-            skelPoz1x =((int)(Math.random() * (9 - 1 + 1)) + 1);
-            skelPoz1y = ((int)(Math.random() * (9 - 1 + 1)) + 1);
+        while (map [skelPoz1y][skelPoz1x] !=1 ) {
+            skelPoz1x =((int)(Math.random() * (9 - 3 + 1)) + 3);
+            skelPoz1y = ((int)(Math.random() * (9 - 3 + 1)) + 3);
         }
-        PositionedImage skeleton1 = new PositionedImage("img/skeleton.png", skelPoz1x*72, skelPoz1y*72);
+        PositionedImage skeleton1 = new PositionedImage("img/skeleton.png", skelPoz1x*tileSize, skelPoz1y*tileSize);
         skeleton1.draw(graphics);
 
-        while (map [skelPoz2y][skelPoz2x]== 0 ) {
-            skelPoz2x =((int)(Math.random() * (9 - 1 + 1)) + 1);
-            skelPoz2y = ((int)(Math.random() * (9 - 1 + 1)) + 1);
+        while ((map [skelPoz2y][skelPoz2x] !=1)  ) {
+            skelPoz2x =((int)(Math.random() * (9 - 3 + 1)) + 3);
+            skelPoz2y = ((int)(Math.random() * (9 - 3 + 1)) + 3);
         }
-        PositionedImage skeleton2 = new PositionedImage("img/skeleton.png", skelPoz2x*72, skelPoz2y*72);
+        PositionedImage skeleton2 = new PositionedImage("img/skeleton.png", skelPoz2x*tileSize, skelPoz2y*tileSize);
         skeleton2.draw(graphics);
 
-        while (map [skelPoz3y][skelPoz3x]== 0 ) {
-            skelPoz3x =((int)(Math.random() * (9 - 1 + 1)) + 1);
-            skelPoz3y = ((int)(Math.random() * (9 - 1 + 1)) + 1);
+        while (map [skelPoz3y][skelPoz3x] !=1 ) {
+            skelPoz3x =((int)(Math.random() * (9 - 3 + 1)) + 3);
+            skelPoz3y = ((int)(Math.random() * (9 - 3 + 1)) + 3);
         }
-        PositionedImage skeleton3 = new PositionedImage("img/skeleton.png", skelPoz3x*72, skelPoz3y*72);
+        PositionedImage skeleton3 = new PositionedImage("img/skeleton.png", skelPoz3x*tileSize, skelPoz3y*tileSize);
         skeleton3.draw(graphics);
+
+        while (map [bossPozY][bossPozX] !=1 ) {
+            bossPozX =((int)(Math.random() * (9 - 3 + 1)) + 3);
+            bossPozX = ((int)(Math.random() * (9 - 3 + 1)) + 3);
+        }
+        PositionedImage boss = new PositionedImage("img/boss.png", bossPozX*tileSize, bossPozY*tileSize);
+        boss.draw(graphics);
 
     }
 
@@ -140,22 +147,21 @@ public class Board extends JComponent implements KeyListener {
 
     public void keyReleased(KeyEvent e) {
         // When the up or down keys hit, we change the position of our box
-        if (e.getKeyCode() == KeyEvent.VK_DOWN && bossY < boardSize - tileSize && map[row + 1][coulumn] == 1) {
-            bossY += increment;
+        if (e.getKeyCode() == KeyEvent.VK_DOWN && heroY < boardSize - tileSize && map[row + 1][column] == 1) {
+            heroY += increment;
             row += 1;
-        } else if (e.getKeyCode() == KeyEvent.VK_UP && bossY > 0 && map[row - 1][coulumn] == 1 && row != 0) {
-            bossY -= increment;
+        } else if (e.getKeyCode() == KeyEvent.VK_UP && heroY > 0 && map[row - 1][column] == 1 && row != 0) {
+            heroY -= increment;
             row -= 1;
-        } else if (e.getKeyCode() == KeyEvent.VK_LEFT && bossX > 0 && map[row][coulumn - 1] == 1 && coulumn != 0) {
-            bossX -= increment;
-            coulumn -= 1;
-        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && bossX < boardSize - tileSize && map[row][coulumn + 1] == 1 && (coulumn + 1) != 0) {
-            bossX += increment;
-            coulumn += 1;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT && heroX > 0 && map[row][column - 1] == 1 && column != 0) {
+            heroX -= increment;
+            column -= 1;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && heroX < boardSize - tileSize && map[row][column + 1] == 1 && (column + 1) != 0) {
+            heroX += increment;
+            column += 1;
         }
 
-
-        // and redraw to have a new picture with the new coordinates
+        // and redraw to have a new picture with the new coordinate
         repaint();
     }
 
